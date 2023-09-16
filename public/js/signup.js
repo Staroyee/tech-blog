1de07d34e3d2006de26e27e11797553f37e47673
Staroyee <danmasefield@gmail.com>
2023-09-16 21:22:10 +0930

    Created signup views, logout and signup js functions

---
+++ public/js/signup.js
@@ -0,0 +1,26 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+
+    const user_name = document.querySelector('#username-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (user_name && email && password) {
+        const res = await fetch('/api/users', {
+            method: 'post',
+            body: JSON.stringify({
+                user_name,
+                email,
+                password
+            }),
+            headers: {'Content-Type': 'application/json'}
+        });
+        if (res.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(res.statusText)
+        }
+    }
+}
+
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
