960892b9885abc15041cca6c9de335b326607e4d
Staroyee <danmasefield@gmail.com>
2023-09-17 10:31:21 +0930

    Created homepage view, fixed helpers bug in server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const routes = require('./controllers');
 const path = require('path');
-const hbs = exphbs.create({});
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers});
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
