298215e33e7a4b84511900d5706465add0e43016
Staroyee <danmasefield@gmail.com>
2023-09-19 17:16:10 +0930

    created delete-article.js content

---
+++ public/js/delete-article.js
@@ -0,0 +1,17 @@
+async function deleteFormHandler(event) {
+    event.preventDefault();
+
+    const id = window.location.toString().split('/')[
+        window.location.toString().split('/').length -1
+    ];
+    const res = await fetch(`/api/articles/${id}`, {
+        method: 'DELETE',
+    });
+    if (res.ok) {
+        document.location.replace('/dashboard');
+    } else {
+        alert(res.statusText);
+    }
+};
+
+document.querySelector('.delete-article-btn').addEventListener('click', deleteFormHandler);
